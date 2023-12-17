d18004b28eb701d53a834668a23a4ca06ae9c021
hidems <miura.eleven@gmail.com>
2023-12-17 22:36:01 +0100

    Copy files

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-// import { createPinia } from 'pinia'
-import App from './App.vue'
+import App from './components/App.vue'
 import store from './store'
 
 const app = createApp(App)
 
-// app.use(createPinia())
 app.use(store)
 app.mount('#app')
