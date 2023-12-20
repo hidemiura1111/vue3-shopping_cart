1c5eea920484ae1eeee3708ba832d441ec525be2
hidems <miura.eleven@gmail.com>
2023-12-20 21:21:09 +0100

    Check curerncy.js

--- src/api/shop.js
+++ src/api/shop.js
@@ -5,7 +5,7 @@ const _products = [
   { 'id': 1, 'title': 'iPad 4 Mini', 'price': 500.01, 'inventory': 2 },
   { 'id': 2, 'title': 'H&M T-Shirt White', 'price': 10.99, 'inventory': 10 },
   { 'id': 3, 'title': 'Charli XCX - Sucker CD', 'price': 19.99, 'inventory': 5 },
-  { 'id': 4, 'title': 'Pixel 4', 'price': 100, 'inventory': 1 }
+  { 'id': 4, 'title': 'House', 'price': 10050000500, 'inventory': 1 }
 ]
 
 export default {
--- src/currency.js
+++ src/currency.js
@@ -1,22 +1,35 @@
 const digitsRE = /(\d{3})(?=\d)/g
 
 export function currency(value, currency, decimals) {
+  // Ex. value: 30500500.500
   value = parseFloat(value)
+
   if (!isFinite(value) || (!value && value !== 0)) return ''
   currency = currency != null ? currency : '$'
   decimals = decimals != null ? decimals : 2
   var stringified = Math.abs(value).toFixed(decimals)
+
+  // Ex. _int: 30500500
   var _int = decimals
     ? stringified.slice(0, -1 - decimals)
     : stringified
+
+  // Ex. i: 2
   var i = _int.length % 3
+
+  // Ex. head: 30,
   var head = i > 0
     ? (_int.slice(0, i) + (_int.length > 3 ? ',' : ''))
     : ''
+
+  // Ex. _float: .500
   var _float = decimals
     ? stringified.slice(-1 - decimals)
     : ''
+
   var sign = value < 0 ? '-' : ''
+
+  // Ex. _int.slice(i).replace(digitsRE, '$1,')): 500,500
   return sign + currency + head +
     _int.slice(i).replace(digitsRE, '$1,') +
     _float
