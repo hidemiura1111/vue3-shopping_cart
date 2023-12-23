9efd636a463ccc6d68d11d83cc61150655474384
hidems <miura.eleven@gmail.com>
2023-12-23 22:44:49 +0100

    WantList, Add base files

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,21 +2,39 @@
   <div id="app">
     <h1>Shopping Cart Example</h1>
     <hr>
-    <h2>Products</h2>
-    <ProductList/>
-    <hr>
+    <div class="container">
+      <h2>Products</h2>
+      <ProductList/>
+      <hr>
+      <h2>Want List</h2>
+      <WantList/>
+      <hr>
+    </div>
     <ShoppingCart/>
   </div>
 </template>
 
 <script>
 import ProductList from './ProductList.vue'
+import WantList from './WantList.vue'
 import ShoppingCart from './ShoppingCart.vue'
 
 export default {
   components: {
     ProductList,
+    WantList,
     ShoppingCart
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: white;
+}
+.container {
+}
+</style>
---
+++ src/components/WantList.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li>test 1</li>
+    <li>test 1</li>
+    <li>test 1</li>
+    <li>test 1</li>
+  </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
